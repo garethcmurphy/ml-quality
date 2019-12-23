d0a9236082eb5775d38a11213d690a837f696f4c
Gareth Murphy <garethcmurphy@gmail.com>
2019-12-23 06:33:42 +0000

    add depenciences

--- quality.py
+++ quality.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python3
+"""test ml"""
 from __future__ import absolute_import, division, print_function, unicode_literals
 
 import numpy as np
@@ -8,3 +10,15 @@ import tensorflow as tf
 from tensorflow import feature_column
 from tensorflow.keras import layers
 from sklearn.model_selection import train_test_split
+
+
+URL = 'https://storage.googleapis.com/applied-dl/heart.csv'
+dataframe = pd.read_csv(URL)
+dataframe.head()
+
+
+train, test = train_test_split(dataframe, test_size=0.2)
+train, val = train_test_split(train, test_size=0.2)
+print(len(train), 'train examples')
+print(len(val), 'validation examples')
+print(len(test), 'test examples')
