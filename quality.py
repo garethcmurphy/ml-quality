bc618c1b26cc54197279b38e9dc4106ba25b41bb
Gareth Murphy <garethcmurphy@gmail.com>
2019-12-23 06:37:14 +0000

    ad utility

--- quality.py
+++ quality.py
@@ -11,14 +11,28 @@ from tensorflow import feature_column
 from tensorflow.keras import layers
 from sklearn.model_selection import train_test_split
 
+def main():
 
-URL = 'https://storage.googleapis.com/applied-dl/heart.csv'
-dataframe = pd.read_csv(URL)
-dataframe.head()
+    URL = 'https://storage.googleapis.com/applied-dl/heart.csv'
+    dataframe = pd.read_csv(URL)
+    dataframe.head()
 
 
-train, test = train_test_split(dataframe, test_size=0.2)
-train, val = train_test_split(train, test_size=0.2)
-print(len(train), 'train examples')
-print(len(val), 'validation examples')
-print(len(test), 'test examples')
+    train, test = train_test_split(dataframe, test_size=0.2)
+    train, val = train_test_split(train, test_size=0.2)
+    print(len(train), 'train examples')
+    print(len(val), 'validation examples')
+    print(len(test), 'test examples')
+
+
+def df_to_dataset(dataframe, shuffle=True, batch_size=32):
+    dataframe = dataframe.copy()
+    labels = dataframe.pop('target')
+    ds = tf.data.Dataset.from_tensor_slices((dict(dataframe), labels))
+    if shuffle:
+        ds = ds.shuffle(buffer_size=len(dataframe))
+    ds = ds.batch(batch_size)
+    return ds
+
+if __name__ == "__main__":
+    main()
