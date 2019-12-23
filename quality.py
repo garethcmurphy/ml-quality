c611b1f517fee646f7930f70e925e6c1397de6c3
Gareth Murphy <garethcmurphy@gmail.com>
2019-12-23 06:30:48 +0000

    add ipenv

---
+++ quality.py
@@ -0,0 +1,10 @@
+from __future__ import absolute_import, division, print_function, unicode_literals
+
+import numpy as np
+import pandas as pd
+
+import tensorflow as tf
+
+from tensorflow import feature_column
+from tensorflow.keras import layers
+from sklearn.model_selection import train_test_split
