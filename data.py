703a8551e5861db3182d9806bb0baf71455fa735
Gareth Murphy <garethcmurphy@gmail.com>
2019-12-25 23:19:28 +0000

    add random init

--- data.py
+++ data.py
@@ -1,12 +1,20 @@
 #!/usr/bin/env python3
+"""generate data"""
+import random
 
 class Data():
+    """generate data"""
     def __init__(self):
         pass
 
     def generate(self):
-        for i  in range(1,10):
-            print("{}, {}, {}".format(i,i,i))
+        """generate data"""
+        for i in range(1, 10):
+            pid = random.randint( 1e5, 9e5)
+            hindex = random.randint(0, 100)
+            data_type =  random.choice( ['test', 'production'] )
+
+            print("{}, {}, {}".format(pid, hindex, data_type))
 
 
 
